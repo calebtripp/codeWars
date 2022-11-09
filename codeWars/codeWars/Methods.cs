f0070e09f073e75a99593c6b63997616c9b49d8b
calebtripp <calebtripp@outlook.com>
2022-11-09 15:21:43 -0500

    Who Likes It Kata Solved

--- codeWars/codeWars/Methods.cs
+++ codeWars/codeWars/Methods.cs
@@ -1300,4 +1300,25 @@
 //    }
 //}
 
-//
+//Who Likes It Kata
+
+//using System.Collections.Generic;
+//using System.Linq;
+
+//public static class Kata
+//{
+//    public static string Likes(string[] name)
+//    {
+//        int arrayCounter = name.Count();
+
+//        if (name == null || arrayCounter == 0) return "no one likes this";
+
+//        if (arrayCounter == 1) return $"{name[0]} likes this";
+//        if (arrayCounter == 2) return $"{name[0]} and {name[1]} like this";
+//        if (arrayCounter == 3) return $"{name[0]}, {name[1]} and {name[2]} like this";
+
+//        else return $"{name[0]}, {name[1]} and {(arrayCounter -= 2)} others like this";
+//    }
+//}
+
+
